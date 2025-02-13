b198699ceccae8446c1c184010d522699a49c2de
Raghu K <raghuk.vit@gmail.com>
2025-02-13 08:56:59 +0530

    RoboShop Code

--- frontend.sh
+++ frontend.sh
@@ -2,30 +2,33 @@ print_head() {
   echo -e "\e[36m$*\e[0m"
 }
 
+log_file=/tmp/roboshop.log
+rm -f $log_file
+
 print_head Disable Default Nginx
-dnf module disable nginx -y >/tmp/roboshop.log
+dnf module disable nginx -y >> $log_file
 
 print_head Enable Nginx 24
-dnf module enable nginx:1.24 -y >/tmp/roboshop.log
+dnf module enable nginx:1.24 -y >> $log_file
 
 print_head Install Nginx
-dnf install nginx -y >/tmp/roboshop.log
+dnf install nginx -y >> $log_file
 
 print_head Copy Nginx Config file
-cp nginx.conf /etc/nginx/nginx.conf >/tmp/roboshop.log
+cp nginx.conf /etc/nginx/nginx.conf >> $log_file
 
 print_head Clean Old Content
-rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* >> $log_file
 
 print_head Download App content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >> $log_file
 cd /usr/share/nginx/html
 
 print_head Extract app content
-unzip /tmp/frontend.zip >/tmp/roboshop.log
+unzip /tmp/frontend.zip >> $log_file
 
 print_head Start Nginx Service
-systemctl enable nginx >/tmp/roboshop.log
-systemctl restart nginx >/tmp/roboshop.log
+systemctl enable nginx >> $log_file
+systemctl restart nginx >> $log_file
 
 
