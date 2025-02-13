c05b1590864203eb4782c997787228557cd01e91
Raghu K <raghuk.vit@gmail.com>
2025-02-13 08:45:02 +0530

    RoboShop Code

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,30 @@
-echo -e "\e[35mDisable Default Nginx\e[0m"
+print_head() {
+  echo -e "\e[35m$*\e[0m"
+}
+
+print_head Disable Default Nginx
 dnf module disable nginx -y
 
-echo -e "\e[35mEnable Nginx 24\e[0m"
+print_head Enable Nginx 24
 dnf module enable nginx:1.24 -y
 
-echo -e "\e[35mInstall Nginx\e[0m"
+print_head Install Nginx
 dnf install nginx -y
 
-echo -e "\e[35mCopy Nginx Config file\e[0m"
+print_head Copy Nginx Config file
 cp nginx.conf /etc/nginx/nginx.conf
 
-echo -e "\e[35mClean Old Content\e[0m"
+print_head Clean Old Content
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[35mDownload App content\e[0m"
+print_head Download App content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 
-echo -e "\e[35mExtract app content\e[0m"
+print_head Extract app content
 unzip /tmp/frontend.zip
 
-echo -e "\e[35mStart Nginx Service\e[0m"
+print_head Start Nginx Service
 systemctl enable nginx
 systemctl restart nginx
 
