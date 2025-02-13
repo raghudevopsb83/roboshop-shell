8bb4ef968fae0f41de4f03cfe3ba7ce92e3db43c
Raghu K <raghuk.vit@gmail.com>
2025-02-13 09:00:03 +0530

    RoboShop Code

--- frontend.sh
+++ frontend.sh
@@ -1,37 +1,37 @@
 print_head() {
   echo -e "\e[36m$*\e[0m"
-  echo "#################################################" >> $log_file
-  echo -e "\e[36m$*\e[0m" >> $log_file
-  echo "#################################################" >> $log_file
+  echo "#################################################" &>> $log_file
+  echo -e "\e[36m$*\e[0m" &>> $log_file
+  echo "#################################################" &>> $log_file
 }
 
 log_file=/tmp/roboshop.log
 rm -f $log_file
 
 print_head Disable Default Nginx
-dnf module disable nginx -y >> $log_file
+dnf module disable nginx -y &>> $log_file
 
 print_head Enable Nginx 24
-dnf module enable nginx:1.24 -y >> $log_file
+dnf module enable nginx:1.24 -y &>> $log_file
 
 print_head Install Nginx
-dnf install nginx -y >> $log_file
+dnf install nginx -y &>> $log_file
 
 print_head Copy Nginx Config file
-cp nginx.conf /etc/nginx/nginx.conf >> $log_file
+cp nginx.conf /etc/nginx/nginx.conf &>> $log_file
 
 print_head Clean Old Content
-rm -rf /usr/share/nginx/html/* >> $log_file
+rm -rf /usr/share/nginx/html/* &>> $log_file
 
 print_head Download App content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >> $log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
 cd /usr/share/nginx/html
 
 print_head Extract app content
-unzip /tmp/frontend.zip >> $log_file
+unzip /tmp/frontend.zip &>> $log_file
 
 print_head Start Nginx Service
-systemctl enable nginx >> $log_file
-systemctl restart nginx >> $log_file
+systemctl enable nginx &>> $log_file
+systemctl restart nginx &>> $log_file
 
 
