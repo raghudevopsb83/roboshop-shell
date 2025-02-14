713c60079b8f2f6bc6d0dace61342eb8f2ce4934
Raghu K <raghuk.vit@gmail.com>
2025-02-14 07:52:44 +0530

    RoboShop Code

--- common.sh
+++ common.sh
@@ -77,7 +77,7 @@ python_app_setup() {
 print_head() {
   echo -e "\e[36m$*\e[0m"
   echo "#################################################" &>>$log_file
-  echo -e "\e[36m$*\e[0m" &>> &>>$log_file
+  echo -e "\e[36m$*\e[0m" &>>$log_file
   echo "#################################################" &>>$log_file
 }
 
