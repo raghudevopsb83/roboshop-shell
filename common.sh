7c4916c46a21de74a2bbb4fbc287d4a876a94c1b
Raghu K <raghuk.vit@gmail.com>
2025-02-14 08:15:25 +0530

    RoboShop Code

--- common.sh
+++ common.sh
@@ -1,67 +1,59 @@
 systemd_setup() {
   print_head Copy SystemD service file
   cp $component.service /etc/systemd/system/$component.service &>>$log_file
-  echo $?
+  exit_status_print $?
 
   print_head Start Service
   systemctl daemon-reload &>>$log_file
   systemctl enable $component &>>$log_file
   systemctl restart $component &>>$log_file
-  echo $?
+  exit_status_print $?
 }
 
 artifact_download() {
   print_head Add Application User
   useradd roboshop &>>$log_file
-  echo $?
+  exit_status_print $?
 
   print_head Remove existing application code
   rm -rf /app &>>$log_file
-  echo $?
+  exit_status_print $?
 
   print_head Create application directory
   mkdir /app &>>$log_file
-  echo $?
+  exit_status_print $?
 
 
   print_head Download Application Content
   curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip &>>$log_file
-  echo $?
+  exit_status_print $?
   cd /app
 
   print_head Extract Application Content
   unzip /tmp/$component.zip &>>$log_file
-  echo $?
+  exit_status_print $?
 }
 
 
 nodejs_app_setup() {
   print_head Disable NodeJS default version
   dnf module disable nodejs -y &>>$log_file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m >> SUCCESS\e[0m"
-  else
-    echo -e "\e[31m >> FAILURE\e[0m"
-  fi
+  exit_status_print $?
 
   print_head Enable NodeJS 20
   dnf module enable nodejs:20 -y &>>$log_file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m >> SUCCESS\e[0m"
-  else
-    echo -e "\e[31m >> FAILURE\e[0m"
-  fi
+  exit_status_print $?
 
   print_head Install NodeJS
   dnf install nodejs -y &>>$log_file
-  echo $?
+  exit_status_print $?
 
   artifact_download
   cd /app
 
   print_head Install NodeJs Dependencies
   npm install &>>$log_file
-  echo $?
+  exit_status_print $?
 
   systemd_setup
 }
@@ -103,3 +95,11 @@ print_head() {
 
 log_file=/tmp/roboshop.log
 rm -f $log_file
+
+exit_status_print() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m >> SUCCESS\e[0m"
+  else
+    echo -e "\e[31m >> FAILURE\e[0m"
+  fi
+}
